a09468905d54187c2a6615f84c6e9f684ee37559
Treetrain1 <treetrain1@gmail.com>
2022-06-10 16:12:56 -0500

    add client

---
+++ src/main/java/net/frozenblock/crawler/TheCrawlerClient.java
@@ -0,0 +1,13 @@
+package net.frozenblock.crawler;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TheCrawlerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
