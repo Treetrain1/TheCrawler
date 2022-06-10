9f51fad3afcdd0f6047f6e0031efe11b4df4a592
Treetrain1 <treetrain1@gmail.com>
2022-06-10 17:00:18 -0500

    natural spawns

--- src/main/java/net/frozenblock/crawler/registry/RegisterEntities.java
+++ src/main/java/net/frozenblock/crawler/registry/RegisterEntities.java
@@ -1,5 +1,7 @@
 package net.frozenblock.crawler.registry;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.frozenblock.crawler.TheCrawler;
 import net.frozenblock.crawler.entity.Crawler;
@@ -9,12 +11,15 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.BiomeKeys;
 
 public class RegisterEntities {
 
     public static final EntityType<Crawler> CRAWLER = Registry.register(Registry.ENTITY_TYPE, new Identifier(TheCrawler.MOD_ID, "crawler"), FabricEntityTypeBuilder.createMob().spawnGroup(SpawnGroup.MONSTER).entityFactory(Crawler::new).dimensions(EntityDimensions.fixed(1.7F, 2.8F)).fireImmune().defaultAttributes(Crawler::addAttributes).build());
 
     public static void init() {
-
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.MANGROVE_SWAMP),
+                SpawnGroup.MONSTER,
+                RegisterEntities.CRAWLER, 1, 1, 1);
     }
 }
